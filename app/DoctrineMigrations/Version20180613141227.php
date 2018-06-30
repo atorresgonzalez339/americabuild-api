f7e5c93aba839e17fc37a6fe84be21e9c2a9ff73
Angel Torres <atorresgonzalez339@gmail.com>
2018-06-30 13:19:25 -0400

    Include Vendors

--- app/DoctrineMigrations/Version20180613141227.php
+++ app/DoctrineMigrations/Version20180613141227.php
@@ -18,7 +18,7 @@ class Version20180613141227 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE tbpermit ADD userid INT NOT NULL, ADD block  VARCHAR(255) NOT NULL, ADD pbpg  VARCHAR(255) NOT NULL, DROP block, DROP pbpg');
+        $this->addSql('ALTER TABLE tbpermit ADD userid INT NOT NULL, ADD block  VARCHAR(255) NOT NULL, ADD pbpg  VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE tbpermit ADD CONSTRAINT FK_698FBD9CF132696E FOREIGN KEY (userid) REFERENCES tbuser (id)');
         $this->addSql('CREATE INDEX IDX_698FBD9CF132696E ON tbpermit (userid)');
         $this->addSql('ALTER TABLE tbpermit_permit_type ADD area  NUMERIC(15, 8) NOT NULL, ADD length  NUMERIC(15, 8) NOT NULL, DROP area, DROP length, CHANGE description_of_work description_of_work  LONGTEXT NOT NULL');
